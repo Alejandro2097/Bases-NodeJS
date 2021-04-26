437b50415702acbffce057269558fc9d0922b9b7
Alejandro Huertas <alejo12381@hotmail.es>
2021-04-25 22:35:29 -0500

    first comiit

---
+++ app.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+console.clear();
+console.log("===================")
+console.log("    Tabla del 5")
+console.log("===================")
+const base = 3;
+let salida = '';
+for (let i = 0; i <= 10; i++){
+    salida += `${base} X ${i} = ${base*i}\n`;   
+}
+console.log('salida');
+fs.writeFile(`tabla-${base}.txt`, salida, (err) =>{
+     if(err) throw err;
+     console.log(`tabla-${base}.txt creada satisfactoriamente`)
+})
